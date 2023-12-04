cb5ac20ed17b936face6571c734e3520ac9d0e51
Omar Mohamed <61389765+ghanduud@users.noreply.github.com>
2023-12-04 14:14:17 +0000

    stting the hashing

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,13 +20,22 @@ const timeout = function (s) {
 
 // fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
 
-showRecipe();
+
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+
+
+
+// showRecipe();
 
 
 async function showRecipe() {
   try{
+    const id = window.location.hash.slice(1);
+
+    if(!id) return
+
     renderSpinner(recipeContainer);
-    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
     const data = await res.json();
     if(!res.ok) throw new Error(`${data.message} (${res.status})`);
     let {recipe} = data.data
